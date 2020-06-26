d455bbadcc5fa3e4fd798de3ff52ef6092c65b1e
Muflah Nasir <muflahnasir007@gmail.com>
2020-06-26 18:21:02 +0500

    Setup for production is done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const items = require("./routes/api/items")
+const path = require("path")
 
 const app = express()
 
@@ -26,6 +27,16 @@ mongoose
 // Use Items
 
 app.use("/api/items", items)
+
+// Serve static assets if in Production
+
+if(process.env.NODE_ENV === "production") {
+    //Set a static folder
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 // Make app listener
 
